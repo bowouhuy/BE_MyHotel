6164082ffb7256468a518d0ffd75626e02fe24de
Envy-7z <wisnuandrian325@gmail.com>
2021-07-15 10:06:06 +0700

    commit wisnu

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://api.dagdigdug.my.id/';
+$config['base_url'] = 'http://localhost/BE_MyHotel';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,8 @@ $config['base_url'] = 'https://api.dagdigdug.my.id/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
